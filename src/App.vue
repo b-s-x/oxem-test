a2e5135b73368e3d1f885f1e928ad9720432e8c6
b-s-x <b.s.x.mars@gmail.com>
2021-03-25 12:23:52 +0300

    connected store

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main-page/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MainPage from "./pages/Main"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MainPage
+  },
+
+  data() {
+    return {
+
+    }
+  },
 }
 </script>
 
 <style lang="scss">
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+const SMALL_DATA = "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
+const BIG_DATA = "http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    dataSet: [],
   },
   mutations: {
+    settingData(state, data) {
+      state.dataSet = data
+    }
   },
   actions: {
+    async fetchData({commit}, selector = BIG_DATA) {
+      if(selector === 'small') selector = SMALL_DATA
+
+      const result = await fetch(selector)
+      const data = await result.json()
+
+      commit('settingData', data)
+    }
   },
-  modules: {
+  getters: {
+    getDataSet(store) {
+      return store.dataSet
+    }
   }
 })
