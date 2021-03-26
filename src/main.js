c13d1d86fa41d85b798cb6af383f1ea4c19d60d1
b-s-x <b.s.x.mars@gmail.com>
2021-03-26 19:01:03 +0300

    connected vue-pagination

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import Vuelidate from 'vuelidate'
+import VuePaginate from 'vue-paginate'
 
 Vue.use(Vuelidate)
+Vue.use(VuePaginate)
 
 Vue.config.productionTip = false
 
